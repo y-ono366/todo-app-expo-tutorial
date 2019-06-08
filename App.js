1adc463ed4f780900a7772d8c44012e947509175
yusuke ohno <yusuke@ohno.systems>
2019-06-08 14:29:48 +0900

    setup store reducer

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import TodoScreen from './src/TodoScreen'
+import { Provider } from 'react-redux'
+import { store } from './src/store'
 
 export default class App extends React.Component {
   render() {
     return (
-      <TodoScreen/>
+      <Provider store={store}>
+        <TodoScreen/>
+      </Provider>
     );
   }
 }
